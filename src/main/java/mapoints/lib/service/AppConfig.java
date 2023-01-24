7e7a29862896d9820f7c9b2b7c15d1e9bc66d68a
sir-njguna <sir.njuguna@outlook.com>
2023-01-24 15:11:06 +0300

    Defining external config files.

--- src/main/java/mapoints/lib/service/AppConfig.java
+++ src/main/java/mapoints/lib/service/AppConfig.java
@@ -3,44 +3,20 @@ package mapoints.lib.service;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class AppConfig {
     public static final String ADMIN_BASE_URL = "/admin";
     public static final String MERCHANT_BASE_URL = "/merchant";
     public static final String CUSTOMER_BASE_URL = "/customer";
+
     @Bean
     public MessageSource messageSource(){
         ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
         source.addBasenames("classpath:msg/messages");
         return source;
-    }
-
-    @Bean
-    public PropertySourcesPlaceholderConfigurer propertiesSource() {
-        final PropertySourcesPlaceholderConfigurer propertiesConfigurer = new PropertySourcesPlaceholderConfigurer();
-        propertiesConfigurer.setIgnoreResourceNotFound(false);
-
-        final List<Resource> resourceLst = new ArrayList<>();
-
-        resourceLst.add(new ClassPathResource("application.properties"));
-        resourceLst.add(new FileSystemResource("/opt/mapoints/jwt.properties"));
-        resourceLst.add(new FileSystemResource("/opt/mapoints/db.properties"));
-        resourceLst.add(new FileSystemResource("/opt/mapoints/africas-talking-credentials.properties"));
-        resourceLst.add(new FileSystemResource("/opt/mapoints/mpesa-config.properties"));
-
-        propertiesConfigurer.setLocations(resourceLst.toArray(new Resource[]{}));
-
-        return propertiesConfigurer;
     }
 
 
